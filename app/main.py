9b29f85c949d21f0c044a2f14463e13e98ce1476
Redmi <4ukasti@gmail.com>
2025-07-02 23:09:22 +0300

    макет2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from enum import Enum
 from pydantic import BaseModel
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import secrets
 from fastapi.responses import FileResponse
 #from models.transaction import Transaction
 #from api.v1.transactions import TransactionCreate
@@ -17,6 +18,9 @@ class TransactionNew(BaseModel):
     senderId: str
     receiverId: str
 
+class LoginPass(BaseModel):
+    login: str
+    password: str
 
 app = FastAPI()
 
@@ -37,6 +41,29 @@ def read_root(request: Request):
 @app.get("/images/image1.jpg")
 def read_image():
     return FileResponse(path="images/image1.jpg")
+
+@app.get("/login", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@app.post("/api/login")
+async def try_login(rekvi: LoginPass, response: Response):
+    login = "makaka"
+    password = "777"
+    token = secrets.token_hex(32)
+    if rekvi.login == login and rekvi.password == password:
+        response.set_cookie(
+            key="session_id",
+            value=token,
+            httponly=True,
+            max_age=1800,
+            samesite="lax",
+            secure=False
+        )
+        return "Вы успешно авторизованы."
+    #добавить логирование неуспешных попыток авторизации
+    pass
 
 @app.post("/api/transaction")
 async def send_transaction(transaction: TransactionNew):
